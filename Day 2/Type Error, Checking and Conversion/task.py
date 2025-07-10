07321a6835fa7bd89fa243fee3ca6c3103c024a5
Tabrez Alam <tabrezv.2407@gmail.com>
2025-07-10 13:46:48 +0530

    commit

---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,14 @@
+print(len("12345"))
+
+print(type(True))
+print(type("True"))
+print(type(2.34))
+print(type(1234))
+
+username = input(input("Enter your name"))
+lenth_of_name = len(username)
+
+print(type(lenth_of_name)) #int
+print(type("Number of letters in your name: ")) #str
+
+print("Number of letters in your name: " + str(lenth_of_name))
