ef25cbe75c9a4a025c4fa4e65851d2bc8ad6c85f
Tabrez Alam <tabrezv.2407@gmail.com>
2025-07-10 13:52:35 +0530

    commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to Name Band Generator!")
+city = input("Which city did you grew up?\n")
+pet = input("Which pet would you like to take?\n")
+print("Your Band could be: "+city +" "+pet)
